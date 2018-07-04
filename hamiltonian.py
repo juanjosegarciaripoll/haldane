f04cdd5da3a9676515453477ff77fc6e12f291a1
alvaro <alvarorga@hotmail.com>
2018-07-04 17:46:39 +0200

    Adapt the hamiltonian to the new lattice and change notation

--- hamiltonian.py
+++ hamiltonian.py
@@ -16,51 +16,55 @@ class OpenHamiltonian(object):
     Attributes:
         lattice (OpenLattice): lattice on which the Hamiltonian is
             defined.
-        J (2darray of floats): hopping matrix.
+        A (2darray of floats): hopping matrix.
 
     """
 
-    def __init__(self, lattice, single_levels, hopping_params, hoppings_type):
+    def __init__(self, lattice, hopping_params, hoppings_type,
+                 trap_potential, lattice_imbalance):
         """Initialize the Hamiltonian class.
 
         Args:
             lattice (OpenLattice): lattice on which the Hamiltonian is
                 defined.
-            single_levels (1darray of floats): single particle levels.
-            hopping_params (list of floats): hopping parameters, explained
-                below.
+            hopping_params (tuple of floats): hopping parameters,
+                explained below.
             hoppings_type: string with the name of the hoppings we want
                 to use:
-                'juanjo': juanjo hopping hamiltonian:
-                    hopping_param = [t, ta, tb, dpy].
-                'haldane': haldane's hamiltonian.
-                    hopping_param = [t1, t2, dphi].
-                'graphene': similar to haldane but with real hoppings.
-                    hopping_param = [t1, t2].
+                'juanjo': modified Haldane Hamiltonian:
+                    hopping_param = (t, ta, tb, dpy).
+                'haldane': Haldane's Hamiltonian.
+                    hopping_param = (t1, t2, dphi).
+            trap_potential (float): k coefficient of the harmonic trap.
+                The potential is quadratic like: V(x) = k*x**2, with
+                x the distance of a point in the lattice to the center.
+            lattice_imbalance (float): energy difference between the
+                two sublattices.
         """
         self.lattice = lattice
-        self.J = np.diag(single_levels.astype(np.complex128))
-        self.build_J(hopping_params, hoppings_type)
+        self.A = np.zeros((self.lattice.L, self.lattice.L), np.complex128)
+        self.build_A(hopping_params, hoppings_type,
+                     trap_potential, lattice_imbalance)
 
-    def build_J(self, hopping_params, hoppings_type):
-        """Add the proper hoppings to J.
+    def build_A(self, hopping_params, hoppings_type,
+                trap_potential, lattice_imbalance):
+        """Compute the hopping matrix A."""
+        # Diagonal terms.
+        # Harmonic trap.
+        center = (self.lattice.coords_pts[0] + self.lattice.coords_pts[-1])/2
+        for i in range(self.lattice.L):
+            i_coords = self.lattice.coords_pts[i]
+            self.A[i, i] += trap_potential*np.linalg.norm(i_coords - center)**2
 
-        Args:
-            hopping_params: list of hopping parameters. Explained below.
-            hoppings_type: string with the name of the hoppings we want
-                to use:
-                'juanjo': juanjo hopping hamiltonian:
-                    hopping_param = [t, ta, tb, dpy].
-                'haldane': haldane's hamiltonian.
-                    hopping_param = [t1, t2, dphi].
-                'graphene': similar to haldane but with real hoppings.
-                    hopping_param = [t1, t2].
-                'hubbard_chain': spinful 1D chain with Hubbard
-                    interactions.
-                    hopping_param = [t].
-        """
+        # Lattice imbalance.
+        if not np.isclose(lattice_imbalance, 0):
+            for i in range(self.lattice.L):
+                self.A[i, i] += (lattice_imbalance if i%2 == 0
+                                 else -lattice_imbalance)
+
+        # Hopping terms.
         # Assert hoppings are valid.
-        hoppings_type_list = ['juanjo', 'haldane', 'graphene']
+        hoppings_type_list = ['juanjo', 'haldane']
         if hoppings_type not in hoppings_type_list:
             raise RuntimeError('{} is not a valid hopping.'
                                .format(hoppings_type))
@@ -79,23 +83,17 @@ class OpenHamiltonian(object):
 
         elif hoppings_type == 'haldane':
             t1, t2, dphi = hopping_params
-            ts = t2*np.exp(1j*dphi*np.pi)
+            tmp = t2*np.exp(1j*dphi*np.pi)
             amp_first_neigh_B = np.full(3, t1, np.float64)
             amp_first_neigh_A = np.full(3, t1, np.float64)
-            amp_second_neigh_B = np.array([ts, np.conj(ts), ts,
-                                           np.conj(ts), ts, np.conj(ts)])
+            amp_second_neigh_B = np.array(
+                [tmp, np.conj(tmp), tmp, np.conj(tmp), tmp, np.conj(tmp)]
+                )
             amp_second_neigh_A = np.conj(amp_second_neigh_B)
 
-        elif hoppings_type == 'graphene':
-            t1, t2 = hopping_params
-            amp_first_neigh_B = np.full(3, t1, np.float64)
-            amp_first_neigh_A = amp_first_neigh_B
-            amp_second_neigh_B = np.full(6, t2, np.float64)
-            amp_second_neigh_A = amp_second_neigh_B
-
-        # Add the hoppings to J.
+        # Add the hoppings to A.
         for i in range(self.lattice.L):
-            if i%2 == 1:
+            if i%2 == 0:
                 # Sublattice A.
                 for (delta, amp) in zip(self.lattice.first_neigh_A,
                                         amp_first_neigh_A):
@@ -113,12 +111,10 @@ class OpenHamiltonian(object):
                                         amp_second_neigh_B):
                     self.add_hopping(i, delta, amp)
 
-        # Expand into two spin populations.
-        self.J = np.kron(np.eye(2), self.J)
         return
 
     def add_hopping(self, i, delta, amp):
-        """Add a hopping to the hopping matrix J.
+        """Add a hopping to the hopping matrix A.
 
         Args:
             i (int): index of the initial position of the hopping.
@@ -143,6 +139,6 @@ class OpenHamiltonian(object):
                 not_crosses_boundary = False
 
         if not_crosses_boundary:
-            self.J[j, i] += amp
+            self.A[j, i] += amp
 
         return
