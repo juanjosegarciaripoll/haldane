f3ce85e1530ed16528357630161e96c08aae0dbe
alvaro <alvarorga@hotmail.com>
2018-07-15 12:25:58 +0200

    Add a manual voronoi plot made with matplotlib.fill

--- plot_lattice.py
+++ plot_lattice.py
@@ -1,12 +1,14 @@
 """Different plot scripts for the Honeycomb lattice."""
 
 import numpy as np
+from scipy.spatial import Voronoi
 import matplotlib as mpl
 import matplotlib.cm as cm
 import matplotlib.pyplot as plt
 
 
-def plot_voronoi(lattice, data, colormap='binary', figsize=(20, 12)):
+def plot_voronoi(lattice, data, colormap='binary', figsize=(20, 12),
+                 alpha=1, binary_minmax_data=False):
     """Plot data on a Voronoi diagram of the lattice.
 
     Args:
@@ -20,8 +22,11 @@ def plot_voronoi(lattice, data, colormap='binary', figsize=(20, 12)):
 
     if np.any(np.iscomplex(data)):
         data = np.real(data)
-    norm = mpl.colors.Normalize(vmin=np.min(data), vmax=np.max(data),
-                                clip=True)
+    if binary_minmax_data:
+        norm = mpl.colors.Normalize(vmin=0, vmax=1, clip=True)
+    else:
+        norm = mpl.colors.Normalize(vmin=np.min(data), vmax=np.max(data),
+                                    clip=True)
     mapper = cm.ScalarMappable(norm=norm, cmap=colormap)
 
     fill_coords = np.zeros((lattice.L, 3, 2), np.float64)
@@ -33,5 +38,24 @@ def plot_voronoi(lattice, data, colormap='binary', figsize=(20, 12)):
         else:
             fill_coords[i] = lattice.xy_coords[i] + disp_B
         plt.fill(fill_coords[i, :, 0], fill_coords[i, :, 1],
-                 color=mapper.to_rgba(data[i]))
+                 color=mapper.to_rgba(data[i]), alpha=alpha)
+    return
+
+
+def plot_voronoi_2(lattice, data, colormap='Blues', figsize=(20, 12),
+                   alpha=1, binary_minmax_data=False):
+    """Plot with the help of scipy's Voronoi."""
+    vor = Voronoi(lattice.xy_coords)
+    if binary_minmax_data:
+        norm = mpl.colors.Normalize(vmin=0, vmax=1, clip=True)
+    else:
+        norm = mpl.colors.Normalize(vmin=np.min(data), vmax=np.max(data),
+                                    clip=True)
+    mapper = cm.ScalarMappable(norm=norm, cmap=colormap)
+    for r in range(len(vor.point_region)):
+        region = vor.regions[vor.point_region[r]]
+        if not np.any(np.isclose(-1, region)):
+            polygon = [vor.vertices[i] for i in region]
+            plt.fill(*zip(*polygon), color=mapper.to_rgba(data[r]),
+                     alpha=alpha)
     return
